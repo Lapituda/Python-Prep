44507e585adf47baeb35e312aea4f606af69a208
Angel <angeltod09@gmail.com>
2023-04-25 10:09:16 -0600

    first commit

--- 02 - Variables y Tipos de Datos/Prep_Course_Homework_02-Resuelto.py
+++ 02 - Variables y Tipos de Datos/Prep_Course_Homework_02-Resuelto.py
@@ -18,6 +18,7 @@ print(a)
 
 
 type(8.5)
+print(type)
 
 
 # 3) Imprimir el tipo de dato de la variable creada en el punto 1
@@ -239,4 +240,3 @@ str(2) + str('2')
 var1 = 'este texto se repite '
 var2 = 3
 print(var1 * var2 + str(var2) + ' veces')
-
