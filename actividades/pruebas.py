2423d6ec6c8e82a2470bf1b1d012e22507609db2
Angel <angeltod09@gmail.com>
2023-04-26 09:34:27 -0600

    nuevo cambio

---
+++ actividades/pruebas.py
@@ -0,0 +1,17 @@
+mi_diccionario = {
+    "Colores Primarios": ["Rojo", "Azul", "Amarillo"],
+    "Colores secundarios": ["Naranja", "Violeta", "Verde"],
+    "Clave3": 10,
+    "Clave4": False,
+}
+
+print(
+    mi_diccionario["Colores Primarios"],
+    mi_diccionario["Colores secundarios"],
+    mi_diccionario["Clave3"],
+    mi_diccionario["Clave4"],
+)
+
+print(mi_diccionario.keys())
+
+print(mi_diccionario.values())
